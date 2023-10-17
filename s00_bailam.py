2153d484eff4fd3786b1b32ba76e45b01311dff7
HaDungx <146812707+HaDungx@users.noreply.github.com>
2023-10-17 06:52:17 +0000

    bainop

--- s00_bailam.py
+++ s00_bailam.py
@@ -47,9 +47,10 @@ import re
 def get_24hformat_hour(hour_str):
   time = None
   if hour_str:
-    e = re.findall('am|AM|pm|PM', hour_str)[0]
+    hour_str = hour_str.upper()
+    e = re.findall('AM|PM', hour_str)[0]
     s = re.findall('\d+', hour_str)[0]
-    if e.upper() == 'PM':
+    if e == 'PM':
       time = 12 + int(s)
     else:
       time = int(s)
